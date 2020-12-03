11ea8f4954bf91c12346634532ab0e68f7004f49
mayank-bh22 <75407618+mayank-bh22@users.noreply.github.com>
2020-12-03 11:07:28 +0530

    Add files via upload

---
+++ WebApp2/WebApp2/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp2
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
